424bde6bf57db99cd3a4952384b6e6b2f1de3325
BEUMGOO KIM <captain_kbg@naver.com>
2023-03-19 00:16:53 +0900

    header

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,14 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-    bgColor: "#2f3640",
-    textColor: "white",
-    contrastTextColor: "white",
-    accentColor: "#4cd137",
-    cardBgColor: "ghostwhite",
+export const theme: DefaultTheme = {
+    red: "#E51013",
+    black: {
+        veryDark: "#141414",
+        darker: "#181818",
+        lighter: "#2F2F2F",
+    },
+    white: {
+        lighter: "#fff",
+        darker: "#e5e5e5",
+    },
 };
